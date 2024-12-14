84da39b11fe560b490fedf42392b7151b6a84903
ZL <ray040406@gmail.com>
2024-12-14 16:29:17 +1300

    controllers

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("signup route");
+};
+export const login = (req, res) => {
+  res.send("login route");
+};
+export const logout = (req, res) => {
+  res.send("logout route");
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,9 @@
 import exporess from "express";
-
+import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = exporess.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("signup route");
-});
-
-router.get("/login", (req, res) => {
-  res.send("login route");
-});
-
-router.get("/logout", (req, res) => {
-  res.send("logout route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
