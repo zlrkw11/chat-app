ffec802023b47a886f935808588d2cf9fe63409f
ZL <ray040406@gmail.com>
2024-12-14 23:27:35 +1300

    x

--- backend/src/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export const signup = (req, res) => {
-  res.send("signup route");
-};
-export const login = (req, res) => {
-  res.send("login route");
-};
-export const logout = (req, res) => {
-  res.send("logout route");
-};
--- backend/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import exporess from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
-const router = exporess.Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
-
-export default router;
