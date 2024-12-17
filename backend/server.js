57b8af616122d5918c8b70d5d714b710e0433586
ZL <ray040406@gmail.com>
2024-12-17 23:06:17 +1300

    homepage route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 // express app
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ mssg: "Homepage" });
+});
+
 // listen for requests
 app.listen(4000, () => {
   console.log("listening on port 4000");
